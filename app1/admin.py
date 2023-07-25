dfb5e9b549f3266585c028368a3ad7c4bb4e0326
Julia Kraft <julia.kraft@teilnehmer-bfwsb.de>
2023-07-25 16:40:39 +0200

    Listenübersicht

--- app1/admin.py
+++ app1/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Block)
+# admin.site.register(Block)
+
+@admin.register(Block)
+class BlockAdmin(admin.ModelAdmin):
+    list_display = ['title', 'body', 'author', 'created', 'status']
+    list_filter = ['author', 'status']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    #raw_id_fields = ['author']
+    date_hierarchy = 'published'
+    ordering = ['status', 'published']
